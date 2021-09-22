bf5ff73b27815e905f260253e1386c90b594a1f9
Francisco Jimenez <francisco.jimenez@naranjax.com>
2021-09-22 20:44:20 -0300

    agregamos motor de vistas

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const app = express();
+const path = require('path');
+const ruta = require('./router/home.routes')
+// preparamos server para formularios
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './public')));
+
+
+//motor de vistas para
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, '/views'))
+
+// perparamos las rutas
+app.use('/',ruta)
+
+module.exports = app
